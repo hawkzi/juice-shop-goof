515e50b0298a39dd91a5b837d3be4541da752ce5
hawkzi <helenabinn@gmail.com>
2025-03-27 18:26:54 -0300

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,7 +1,11 @@
 // index.js
+npm install
+
 const countWords = require('./wordCounter');
 
 const sentence = "Olá, como você está?";
 const wordCount = countWords(sentence);
 
 console.log(`A frase tem ${wordCount} palavras.`);
+
+
